944f2af23eb34e4c678d6e036663ee0208178ecc
LbsLightX <shivamkumarking66@gmail.com>
2025-12-27 18:11:51 +0530

    🔩 | Deploy Astro site to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,7 @@
 import { defineConfig } from 'astro/config';
-import nodejs from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-    vite: {
-        build: {
-            minify: false
-        }
-    },
-    output: "hybrid",
-    adapter: nodejs({
-        mode: "standalone"
-    })
+  site: 'https://lbslightx.github.io',
+  base: '/cryptid/',
+  output: 'static',
 });
